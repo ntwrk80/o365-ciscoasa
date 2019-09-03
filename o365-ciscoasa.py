2e3db919b23c787bc2163b33453479036a69bc88
Ben Story <ben.story@gmail.com>
2019-09-03 14:40:14 -0500

    Revert "Fix bug in removing duplicate IPs."
    This reverts commit 92fdfd4265e0a02f991f4abe47baa27a963a0d2d.

--- o365-ciscoasa.py
+++ o365-ciscoasa.py
@@ -20,7 +20,6 @@ def webApiGet(methodName, instanceName, clientRequestId):
 
 def printASA(endpointSets):
     flatIps=[]
-    uniqueIps = []
     with open('O365-CiscoASA-ObjectGroups.txt', 'w') as output:
         for endpointSet in endpointSets:
             if endpointSet['category'] in ('Optimize', 'Allow'):
@@ -29,17 +28,18 @@ def printASA(endpointSets):
                 serviceArea = endpointSet['serviceArea']
                 # IPv4 strings have dots while IPv6 strings have colons
                 ip4s = [ip for ip in ips if '.' in ip]
-                tcpPorts = endpointSet['tcpPorts'] if 'tcpPorts' in endpointSet else ''
-                udpPorts = endpointSet['udpPorts'] if 'udpPorts' in endpointSet else ''
-                #print("Test")
-                #print(ip4s)
+                uniqueIps = []
                 #Strip out duplicate IP Addresses.
                 for currentIp in ip4s:
                     if currentIp not in uniqueIps:
                         uniqueIps.append(currentIp)
+
+                tcpPorts = endpointSet['tcpPorts'] if 'tcpPorts' in endpointSet else ''
+                udpPorts = endpointSet['udpPorts'] if 'udpPorts' in endpointSet else ''
+                #print("Test")
+                #print(ip4s)
                 for ip in uniqueIps:
                     flatIps.extend([(serviceArea, category, ip, tcpPorts, udpPorts)])
-
 
         print("Converting O365 Endpoints into ASA Groups")
         #print (flatIps)
